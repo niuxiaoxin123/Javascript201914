ddd18efc64dad20c26690cdb179b5d3445bdc15a
niuxiaoxin123 <903958706@qq.com>
2020-02-10 17:14:42 +0800

    3

---
+++ 8.正式课第八周/day1/a.js
@@ -0,0 +1,18 @@
+// console.log(199);
+// // a.js在Node环境下运行了
+// // process:node中进程管理的属性
+// //console.log(process.env.NODE_EVN);
+// let n = process.env.NODE_EVN;
+// if(n==="dev"){
+//     console.log("这是开发环境")
+// }else{
+//     console.log("这是开发环境")
+// }
+function toArray(){
+    console.log(100);
+}
+console.log(888);
+
+module.exports={
+    a:toArray
+}
---
+++ 8.正式课第八周/day1/b.js
@@ -0,0 +1,25 @@
+// let a = require("./a");// 是把a.jsmodule.exports导出的对象给了变量a;
+// let fs = require("fs");// fs 这是文件系统模块，里面包含读写文件的方法；
+// a.a();
+//console.log(module);
+//console.log(require);
+//console.log(exports);
+//console.log(__dirname);
+//console.log(__filename)
+
+// 一个js文件就是一个模块；当Node解析时，会把这个文件中的代码包裹到一个自执行函数中，并且给这个自执行函数传递了五个参数
+(function(module,require,exports,__dirname,__filename){
+
+
+
+
+
+
+})()
+
+
+// console.log(global);
+
+
+
+
