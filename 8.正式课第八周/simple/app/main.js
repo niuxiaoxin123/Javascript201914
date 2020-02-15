6b5d9c19c4b5f2c206565ae7033eee8d39d49a6d
niuxiaoxin123 <903958706@qq.com>
2020-02-15 17:19:29 +0800

    3

--- 8.正式课第八周/simple/app/main.js
+++ 8.正式课第八周/simple/app/main.js
@@ -1,2 +1,14 @@
-const greeter = require('./greeter.js');
-document.querySelector("#root").appendChild(greeter());
+// const greeter = require('./greeter.js');
+// document.querySelector("#root").appendChild(greeter());
+// import "../css/index.css";
+import "../css/2.less";
+import img from "../image/node.png";
+let num =100;
+let fn = ()=>{
+    console.log(100);
+}
+fn();
+console.log(img);
+let str = `<img src="${img}"></img>`;
+// Es6的写法
+
